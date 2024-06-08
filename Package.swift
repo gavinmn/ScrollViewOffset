8e042db75e7adfe24817804124de1a47b1df6839
Gavin Nelson <gavin@nelson.co>
2024-06-08 14:24:22 -0700

    Add ScrollViewOffset

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "ScrollViewOffset",
+	platforms: [
+			.iOS(.v13),
+			.macOS(.v10_15)
+		],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
--- Sources/ScrollViewOffset/ScrollViewOffset.swift
+++ Sources/ScrollViewOffset/ScrollViewOffset.swift
@@ -1,2 +1,42 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+
+struct ScrollViewOffset<Content>: View where Content: View {
+	let axes: Axis.Set
+	let content: () -> Content
+	let onScroll: (CGFloat) -> Void
+	
+	init(axes: Axis.Set = .vertical, @ViewBuilder content: @escaping () -> Content, onScroll: @escaping (CGFloat) -> Void) {
+		self.axes = axes
+		self.content = content
+		self.onScroll = onScroll
+	}
+	
+	struct ScrollViewOffsetPreferenceKey: PreferenceKey {
+		typealias Value = CGFloat
+		static var defaultValue: CGFloat { 0 }
+		static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+			value = nextValue()
+		}
+	}
+	
+	var body: some View {
+		ScrollView(axes) {
+			content()
+				.overlay(
+					GeometryReader { geometry in
+						if axes == .horizontal {
+							Color.clear
+								.preference(key: ScrollViewOffsetPreferenceKey.self, value: geometry.frame(in: .named("scrollView")).origin.x)
+						} else {
+							Color.clear
+								.preference(key: ScrollViewOffsetPreferenceKey.self, value: geometry.frame(in: .named("scrollView")).origin.y)
+						}
+					}
+				)
+		}
+		.onPreferenceChange(ScrollViewOffsetPreferenceKey.self) { value in
+			onScroll(value)
+		}
+		.coordinateSpace(name: "scrollView")
+	}
+}
